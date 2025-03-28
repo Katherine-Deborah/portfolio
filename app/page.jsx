36ddb84370d3fc2e6f316010ca0c3036fbdec717
ahohpotato <119962373+ahohpotato@users.noreply.github.com>
2025-03-27 21:39:45 -0700

    refact: reset entire project

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ratione
+        facere deleniti dolorum explicabo velit! Veniam libero earum atque,
+        tenetur adipisci minima eligendi aut repudiandae repellendus quibusdam
+        ut eveniet cum.
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
